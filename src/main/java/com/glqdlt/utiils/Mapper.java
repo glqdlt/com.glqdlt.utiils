9d839ef985c10d09257e7f6d10f596a090222aba
jhun <jhun@nm-4ones.com>
2019-02-18 12:12:52 +0900

    init

---
+++ src/main/java/com/glqdlt/utiils/Mapper.java
@@ -0,0 +1,10 @@
+package com.glqdlt.utiils;
+
+/**
+ * @author Jhun
+ * 2019-02-18
+ */
+public class Mapper {
+
+
+}
---
+++ src/test/java/com/glqdlt/utiils/MapperTest.java
@@ -0,0 +1,17 @@
+package com.glqdlt.utiils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Jhun
+ * 2019-02-18
+ */
+public class MapperTest {
+
+    @Test
+    public void TestReplication() {
+
+    }
+}
