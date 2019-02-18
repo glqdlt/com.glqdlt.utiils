6ff66d2f3dc0426bdc50d99f46a9a1e680d67c1d
jhun <jhun@nm-4ones.com>
2019-02-18 12:27:04 +0900

    간단한 리플렉션

--- src/test/java/com/glqdlt/utiils/MapperTest.java
+++ src/test/java/com/glqdlt/utiils/MapperTest.java
@@ -2,7 +2,10 @@ package com.glqdlt.utiils;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.stream.Stream;
 
 /**
  * @author Jhun
@@ -12,6 +15,31 @@ public class MapperTest {
 
     @Test
     public void TestReplication() {
+        TestTargetObject testTargetObject = new TestTargetObject();
+        testTargetObject.setBooleanField(true);
+        testTargetObject.setStringField("hello");
+        testTargetObject.setIntegerField(999);
 
+        Field[] feilds = TestTargetObject.class.getDeclaredFields();
+        Method[] methods = TestTargetObject.class.getDeclaredMethods();
+        Stream.of(feilds).forEach(x -> {
+            System.out.println(x.getName());
+        });
+
+        final String regex = "^(is|get)\\w+";
+
+        Stream.of(methods).forEach(x -> {
+            String m = x.getName();
+            boolean matched = m.matches(regex);
+            if(matched){
+                System.out.println(m);
+                try {
+                    Object aaa = x.invoke(testTargetObject);
+                    System.out.println(aaa);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
---
+++ src/test/java/com/glqdlt/utiils/TestTargetObject.java
@@ -0,0 +1,35 @@
+package com.glqdlt.utiils;
+
+/**
+ * @author Jhun
+ * 2019-02-18
+ */
+public class TestTargetObject {
+    public String getStringField() {
+        return stringField;
+    }
+
+    public void setStringField(String stringField) {
+        this.stringField = stringField;
+    }
+
+    public Integer getIntegerField() {
+        return integerField;
+    }
+
+    public void setIntegerField(Integer integerField) {
+        this.integerField = integerField;
+    }
+
+    public boolean isBooleanField() {
+        return booleanField;
+    }
+
+    public void setBooleanField(boolean booleanField) {
+        this.booleanField = booleanField;
+    }
+
+    private String stringField;
+    private Integer integerField;
+    private boolean booleanField;
+}
